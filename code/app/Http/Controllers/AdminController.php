8ac81b62bc14e9b1d500dfbab8cc977f432bb935
daixinyan <381675152@qq.com>
2017-10-25 10:27:45 +0800

    Dev: add views.

---
+++ code/app/Http/Controllers/AdminController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: darxan
+ * Date: 2017/10/25
+ * Time: 10:24
+ */
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function login(Request $request)
+    {
+
+    }
+
+    public function view(Request $request)
+    {
+
+    }
+
+    public function add(Request $request)
+    {
+
+    }
+
+    public function delete(Request $request)
+    {
+
+    }
+
+    public function modify(Request $request)
+    {
+
+    }
+
+    public function searchByName(Request $request)
+    {
+
+    }
+}
---
+++ code/resources/views/user/album.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: darxan
+ * Date: 2017/10/25
+ * Time: 10:21
+ */
